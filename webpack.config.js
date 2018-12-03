b99c9cc809343efcbc2798cd54a372955715ad45
Adaane <adaane.ahamada@adotmob.com>
2018-12-03 12:28:45 +0100

    config biss

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,19 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", 
+          "css-loader",
+
+        ]
       },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
     ]
   }
 };
